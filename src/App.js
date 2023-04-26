a2ccee9596d6c19d87518766c3e39efa3ad388e2
Mahmoud Hawash <hamahmoud768@gmail.com>
2023-04-26 21:53:24 +0200

    Updated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import food from './component/food/food'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <food/>
+      </div>
+    )
+  }
+}
---
+++ src/component/food/food.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import img1 from './imgh/dish-1.png'
+export default class food extends Component {
+    render() {
+        return (
+            <div>
+                <h1>tfgshwd</h1>
+                <img src={img1}/>
+            </div>
+        )
+    }
+}
